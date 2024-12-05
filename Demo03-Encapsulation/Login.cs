c58373b243e4fc3889d5620d2aae9a170bdf22aa
Samuel Legrain <samuel.legrain@bstorm.be>
2024-12-05 14:55:27 +0100

    Add Demo03 - Encapsulation & Properties

---
+++ Demo03-Encapsulation/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo03_Encapsulation
+{
+    public class Login
+    {
+        private string _email;
+
+        public string Email
+        {
+            get {
+                return _email.ToLower();
+            }
+            set {
+                //Plus tard remplacer les "return" par des Exceptions
+                if (value is null) return; 
+                value = value.Trim();
+                if (value.Length > 320) return;
+                if (value.IndexOf('@') < 1 && value.IndexOf('@') > value.Length-1) return; 
+                _email = value;
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            private get 
+            {
+                return _password;
+            }
+            set
+            {
+                if (value is null) return;
+                value = value.Trim();
+                if (value.Length < 8 || value.Length > 32) return;
+                _password = value;
+            }
+        }
+
+        public bool CheckIdentity(string email, string password)
+        {
+            return Email == email && Password == password;
+        }
+
+    }
+}
---
+++ Demo03-Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Demo03_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login user1 = new Login() { 
+                Email = "samuel.legrain@bstorm.be"
+            };
+
+            user1.Password = "Test1234=";
+
+
+
+            Console.WriteLine($"Votre email est {user1.Email}");
+            //Impossible à lire pour la propriété Password, elle est WriteOnly
+            //Console.WriteLine($"Votre mot de passe est {user1.Password}");
+
+            string inputEmail, inputPassword;
+            do
+            {
+                Console.WriteLine($"Veuillez indiquer votre email :");
+                inputEmail = Console.ReadLine();
+                Console.WriteLine($"Veuillez mot de passe :");
+                inputPassword = Console.ReadLine();
+            } while (!user1.CheckIdentity(inputEmail, inputPassword));
+        }
+    }
+}
