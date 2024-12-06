6b299d89147da98cd5670b826f80b897f9dd060d
Samuel Legrain <samuel.legrain@bstorm.be>
2024-12-06 11:19:46 +0100

    Add Demo04-Constructor

---
+++ Demo04-Constructeur/Animal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo04_Constructeur
+{
+    internal class Animal
+    {
+        public string Nom { get; set; }
+        public int PointDeVie { get; private set; }
+        public int PointDeBonheur { get; private set; }
+        private List<string> _jouets;
+        public string[] Jouets
+        {
+            get { return _jouets.ToArray(); }
+        }
+
+        public Animal(string nom) : this(nom, 10, 3, new string[0])
+        {
+        }
+
+        public Animal(string nom, int pv, int pb, string[] jouets) {
+            _jouets = new List<string>(jouets);
+            PointDeVie = pv;
+            PointDeBonheur = pb;
+            Nom = nom;
+        }
+
+        public void Jouer(string jouet)
+        {
+            if (jouet is null) return;
+            if (_jouets.Contains(jouet)) PointDeBonheur++;
+            else PointDeBonheur--;
+            /*OU
+            PointDeBonheur += (_jouets.Contains(jouet)) ? PointDeBonheur + 1 : PointDeBonheur - 1;
+            */
+            /*OU
+            PointDeBonheur += (_jouets.Contains(jouet)) ? 1 : -1;
+            */
+        }
+
+        public void OffrirJouet(string jouet)
+        {
+            if (jouet is null)
+            {
+                PointDeBonheur = 0;
+                return;
+            }
+            if (_jouets.Contains(jouet)) PointDeBonheur++;
+            else
+            {
+                PointDeBonheur += 2;
+                _jouets.Add(jouet);
+            }
+        }
+
+        public void Manger()
+        {
+            PointDeVie++;
+        }
+
+    }
+}
---
+++ Demo04-Constructeur/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Demo04_Constructeur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animalDeCompagnie = new Animal("Bugs");
+
+            Console.WriteLine($"Voici les propriété de notre animal :");
+            Console.WriteLine($"Nom : {((animalDeCompagnie.Nom is null)? "NULL" : animalDeCompagnie.Nom)}");
+            Console.WriteLine($"Point de Vie : { animalDeCompagnie.PointDeVie }");
+            Console.WriteLine($"Point de Bonheur : { animalDeCompagnie.PointDeBonheur }");
+            Console.WriteLine($"Liste de jouets :");
+            foreach (string jouet in animalDeCompagnie.Jouets)
+            {
+                Console.WriteLine($"\t- {jouet}");
+            }
+
+            Animal animalDeSauvegarde = new Animal("Bugs", 5, 0, ["balle","carotte kuin-kuin","grelot géant"]);
+
+            Console.WriteLine($"Voici les propriété de notre animal :");
+            Console.WriteLine($"Nom : {((animalDeSauvegarde.Nom is null) ? "NULL" : animalDeSauvegarde.Nom)}");
+            Console.WriteLine($"Point de Vie : {animalDeSauvegarde.PointDeVie}");
+            Console.WriteLine($"Point de Bonheur : {animalDeSauvegarde.PointDeBonheur}");
+            Console.WriteLine($"Liste de jouets :");
+            foreach (string jouet in animalDeSauvegarde.Jouets)
+            {
+                Console.WriteLine($"\t- {jouet}");
+            }
+        }
+    }
+}
