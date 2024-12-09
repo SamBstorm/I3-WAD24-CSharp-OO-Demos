f1d1379718c1dbcb7af60dfb87453212950cf43e
Samuel Legrain <samuel.legrain@bstorm.be>
2024-12-09 15:08:14 +0100

    Add Demo06-Operators

---
+++ Demo06-Operators/Models/Ingredient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06_Operators.Models
+{
+    public class Ingredient
+    {
+        public string Nom { get; private set; }
+        public int ValeurNutritive { get; private set; }
+
+        public Ingredient(string nom, int valeurNutritive) {
+            Nom = nom;
+            ValeurNutritive = valeurNutritive;
+        }
+
+        public static Recette operator +(Ingredient left, Ingredient right){
+            Recette nouvelleRecette = new Recette($"{left.Nom} avec {right.Nom}");
+            nouvelleRecette.AjouterIngredient(left);
+            nouvelleRecette.AjouterIngredient(right);
+            return nouvelleRecette;
+        }
+
+        public static Recette operator +(Recette left, Ingredient right)
+        {
+            left.AjouterIngredient(right);
+            return left;
+        }
+
+        public static int operator *(Ingredient left, int right)
+        {
+            return left.ValeurNutritive * right;
+        }
+
+        public static bool operator ==(Ingredient left, Ingredient right)
+        {
+            return left.Nom == right.Nom && left.ValeurNutritive == right.ValeurNutritive;
+        }
+
+        public static bool operator !=(Ingredient left, Ingredient right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ Demo06-Operators/Program.cs
@@ -0,0 +1,44 @@
+﻿using Demo06_Operators.Models;
+
+namespace Demo06_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingredient tartine = new Ingredient("Tartine", 100);
+            Console.WriteLine($"Mon premier ingrédient est {tartine.Nom} avec pour valeur nutritive {tartine.ValeurNutritive}");
+            Ingredient choco = new Ingredient("Pâte à tartiner chocolatée", 60);
+            Console.WriteLine($"Mon second ingrédient est {choco.Nom} avec pour valeur nutritive {choco.ValeurNutritive}");
+            Recette tartineAuChoco = tartine + choco;
+            tartineAuChoco += choco;
+            Console.WriteLine($"En mélangeant les ingrédients suivant :");
+            foreach (Ingredient ingredient in tartineAuChoco.Ingredients)
+            {
+                Console.WriteLine($"\t- {ingredient.Nom}");
+            }
+            Console.WriteLine($"J'obtiens la recette de : {tartineAuChoco.Nom} !");
+            Console.WriteLine($"Qui a un totale de valeur nutritive de {tartineAuChoco.ValeurNutritiveTotale} !");
+
+            Console.WriteLine($"Si je prends une quantité de 3 de {tartine.Nom}, j'obtiens une valeur nutritive de {tartine * 3}.");
+
+            if(tartine == choco)
+            {
+                Console.WriteLine("Alors tartine vaut choco");
+            }
+
+            Ingredient tartine2 = tartine;
+
+            if (tartine2 == tartine)
+            {
+                Console.WriteLine("Alors tartine2 vaut tartine");
+            }
+
+            Ingredient tartine3 = new Ingredient("Tartine", 100);
+            if (tartine3 == tartine)
+            {
+                Console.WriteLine("Alors tartine3 vaut tartine");
+            }
+        }
+    }
+}
