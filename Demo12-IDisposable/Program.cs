3a4f9fcb8a5ee0766d8847d5fd095a6aa5cdcde4
Samuel Legrain <samuel.legrain@bstorm.be>
2024-12-13 10:17:24 +0100

    Add Demo12-IDisposable

---
+++ Demo12-IDisposable/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo12_IDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TalkieWalkie tw1 = new TalkieWalkie("Samuel"), tw2 = new TalkieWalkie("Les wads"))
+            {
+                tw1.EmettreMessage("Bonjour à vous les Wad!");
+                tw2.EmettreMessage("Bonjour Samuel!");
+                tw1.EmettreMessage("Les destructeurs, c'est pas top!");
+                tw1.EmettreMessage("Préférez les variables IDisposable!");
+                tw2.EmettreMessage("Tu as raison! Nous ne ferons pas de destructeur!");
+            }
+        }
+    }
+}
---
+++ Demo12-IDisposable/TalkieWalkie.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo12_IDisposable
+{
+    internal class TalkieWalkie : IDisposable
+    {
+        public string UserName { get; set; }
+
+        public TalkieWalkie(string userName)
+        {
+            UserName = userName;
+        }
+        public void EmettreMessage(string message)
+        {
+            Console.WriteLine($"{UserName} {DateTime.Now.ToShortTimeString()} : {message}");
+            Console.WriteLine("À vous!");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine($"C'était {UserName} : Terminé! {DateTime.Now.ToShortTimeString()}");
+        }
+        /*DESTRUCTEUR A NE PLUS UTILISER
+        ~TalkieWalkie()
+        {
+            Dispose();
+        }
+        */
+    }
+}
