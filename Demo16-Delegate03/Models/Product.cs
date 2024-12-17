584f20caae44e03c81a3de502cb7b4f65e163cb0
Samuel Legrain <samuel.legrain@bstorm.be>
2024-12-17 14:32:08 +0100

    Add Demo16-Delegate03

---
+++ Demo16-Delegate03/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo16_Delegate03.Models
+{
+    public class Product
+    {
+        public Guid InventoryCode { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string[] Categories { get; private set; }
+
+        public Product(string name, decimal price, string[] categories) {
+            Name = name;
+            Price = price;
+            Categories = categories;
+            InventoryCode = Guid.NewGuid();
+        }
+    }
+}
---
+++ Demo16-Delegate03/Program.cs
@@ -0,0 +1,37 @@
+﻿using Demo16_Delegate03.Models;
+
+namespace Demo16_Delegate03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Shop shopping = new Shop(
+                new Dictionary<Product, int>(){
+                    { new Product("Snuggie", 39.99M, ["Vêtement","Hiver","Home"]), 4 },
+                    { new Product("Pastelles secs", 24.99M, ["Art","Home","Work"]), 10},
+                    { new Product("Bloc de feuilles 150mg", 12.99M, ["Art","Home","Work"]), 12},
+                    { new Product("Casquette à hélices", 4.99M, ["Vêtement","Fashion","Fun"]), 0},
+                    { new Product("Super dinosaure (peluche de 2.5m)", 199.99M, ["Home","Fun"]), 1},
+                    { new Product("Monopoly Interface3", 19.99M, ["Home","Fun","Jeu"]), 1},
+                    { new Product("Mastermind", 12.99M, ["Home","Fun","Jeu"]), 15}
+                });
+
+            foreach (Product p in shopping.Products)
+            {
+                Console.WriteLine($"- {p.Name} ({p.InventoryCode.ToString().Substring(0,8)}) {p.Price}€");
+            }
+
+            Console.WriteLine($"Quels produits voulez-vous garder?");
+            string productName = Console.ReadLine();
+            Product[] filteredProducts = shopping.FilterProduct(productName);
+            foreach (Product p in filteredProducts)
+            {
+                Console.WriteLine($"- {p.Name} ({p.InventoryCode.ToString().Substring(0, 8)}) {p.Price}€");
+            }
+
+        }
+    }
+}
