6350f2896343afc131e6046d4944a6f53b75e529
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-13 09:57:49 +0100

    Add Demo22 LinQ : Types anonymes

---
+++ Demo22-LinQ-TypeAnonyme/Models/Personne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo22_LinQ_TypeAnonyme.Models
+{
+    internal class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateNaissance { get; set; }
+    }
+}
---
+++ Demo22-LinQ-TypeAnonyme/Program.cs
@@ -0,0 +1,32 @@
+﻿using Demo22_LinQ_TypeAnonyme.Models;
+
+namespace Demo22_LinQ_TypeAnonyme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, veuillez indiquer votre nom :");
+            string nom = Console.ReadLine() ?? "";
+            Console.WriteLine("Veuillez indiquer votre prénom :");
+            string prenom = Console.ReadLine() ?? "";
+            Console.WriteLine("Veuillez indiquer votre date de naissance (au format yyyy-mm-dd) :");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+            //Personne user = new Personne() { Nom = nom, Prenom = prenom, DateNaissance = birthdate};
+            var user = new { nom, prenom, birthdate };
+
+            List<Personne> personnes = new List<Personne>() { 
+                new Personne(){Nom="Legrain", Prenom="Samuel", DateNaissance= new DateTime(1987,9,27)},
+                new Personne(){Nom="Legrain", Prenom="Adrien", DateNaissance= new DateTime(1989,6,21)},
+                new Personne(){Nom="Legrain", Prenom="Corentin", DateNaissance= new DateTime(1991,12,15)}
+            };
+
+            var personnesModifiees = personnes.Select(p => new { p.Prenom, p.DateNaissance });
+            /* //Si nous utilisions les fonctions anonyme au lieu des expressions fléchées (lambda expression)
+            var personnesModifiees = personnes.Select(delegate (Personne p) { return new { p.Prenom, p.DateNaissance }; });
+            var maFonctionFelchee = delegate (Personne p) { return new { p.Prenom, p.DateNaissance }; };
+            */
+        }
+    }
+}
