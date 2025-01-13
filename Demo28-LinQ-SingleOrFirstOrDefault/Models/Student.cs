d852f57c77f26243f5b8cddfe9ca9f5cc1946507
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-13 16:20:16 +0100

    Add Demo28 LinQ : FirstOrDefault/SingleOrDefault

---
+++ Demo28-LinQ-SingleOrFirstOrDefault/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo28_LinQ_SingleOrFirstOrDefault.Models
+{
+    internal class Student
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public Student(string nom, string prenom)
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+    }
+}
---
+++ Demo28-LinQ-SingleOrFirstOrDefault/Program.cs
@@ -0,0 +1,41 @@
+﻿using Demo28_LinQ_SingleOrFirstOrDefault.Models;
+using System.Collections;
+
+namespace Demo28_LinQ_SingleOrFirstOrDefault
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> listEleve = new List<Student>(){
+                new Student ("Legrain", "Samuel"),
+                new Student ("Morre", "Thierry"),
+                new Student ("Person", "Michael"),
+                new Student ("Legrain", "Adrien") };
+
+            Student? s_legrain;
+
+            try
+            {
+                s_legrain = listEleve
+                    .Where((s) => s.Nom == "Legrain" && s.Prenom == "Corentin")
+                    .SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Attention, plus d'un étudiant correspondant!");
+                s_legrain = listEleve
+                    .Where((s) => s.Nom == "Legrain" && s.Prenom == "Corentin")
+                    .FirstOrDefault();
+            }
+            if( s_legrain is null)
+            {
+                Console.WriteLine("Aucune correspondance à la demande...");
+            }
+            else
+            {
+                Console.WriteLine($"Voici l'étudiant : {s_legrain.Prenom} {s_legrain.Nom}.");
+            }
+        }
+    }
+}
